4db3b94962d24f0dbc59d2b339d4a5dcc72b386a
parkerjustice <you@example.com>
2021-12-30 16:33:06 -0500

    added some js and html

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+let initialScreenEl = document.getElementById("initial-screen");
+let questionsEl = document.getElementById("question-sect");
+let scoresEl = document.getElementById("scores");
+let highScoreEl = document.getElementById("high-score");
+let questionAreaEl = document.getElementById("question-area");
+
+
+
+
+
+
+
+
+
+
+
+
+
