2096c049203483ccdf8aa50162f0ed85c7579fde
parkerjustice <you@example.com>
2021-12-30 23:32:52 -0500

    js

--- play.js
+++ play.js
@@ -77,4 +77,44 @@ const progressBarFull = document.querySelector('#progressBarFull');
      const questionsIndex = Math.floor(Math.random() * availableQuestions.length)
      currentQuestion = availableQuestions[questionsIndex]
      question.innerText = currentQuestion.question
+
+     choices.forEach(choice => {
+         const number = choice.dataset['number']
+         choice.innerText = currentQuestion['choice' + number]
+     })
+
+     availableQuestions.splice(questionsIndex, 1)
+
+     accetingAnswers = true
  }
+
+ choices.forEach(choice => {
+     choice.addEventListener('click', e => {
+         if(!accetingAnswers) return
+
+         accetingAnswers = false
+         const selectedChoice = e.target
+         const selectedAnswer = selectedChoice.dataset['number']
+
+         let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' : 'incorrect'
+
+         if(classToApply === 'correct') {
+             incrementScore(SCORE_POINTS)
+         }
+
+         selectedChoice.parentElement.classList.add(classToApply)
+
+         setTimeout(() => {
+             selectedChoice.parentElement.classList.remove(classToApply)
+             getNewQuestion()
+
+         }, 1000)
+     })
+ })
+
+ incrementScore = num => {
+     score +=num
+     scoreText.innerText = score
+ }
+
+ startGame ()
